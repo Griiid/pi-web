78ba46880a752d27a3f385005ebd38f76cc64b1e
griiid <gridwing@gmail.com>
2022-11-25 10:54:45 +0800

    Refactor function mapping

--- line/message_text/message_text.py
+++ line/message_text/message_text.py
@@ -1,20 +1,20 @@
 from .exchange_rate import exchange_rate
 from .uniqlo_price import uniqlo_price
-from utils.print import (
-    print_error,
-    print_item,
-    print_json,
-)
 
-FUNCTION_MAPPING = {
-    "匯率": exchange_rate,
-    "UQ": uniqlo_price,
+_FUNCTION_MAPPING = {}
+_FUNCTION_MAPPING_TEMP = {
+    exchange_rate: ["匯率"],
+    uniqlo_price: ["UQ"],
 }
+
+for func, keys in _FUNCTION_MAPPING_TEMP.items():
+    for key in keys:
+        _FUNCTION_MAPPING[key] = func
 
 
 def process_text(message, reply_token):
     text = message["text"]
     text_split = text.split(" ")
-    func = FUNCTION_MAPPING.get(text_split[0])
+    func = _FUNCTION_MAPPING.get(text_split[0])
     if func:
         func(text_split[1:], reply_token)
